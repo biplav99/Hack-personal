a5f55ddce1c5c09c40159d705f7dbf8407ce9355
graeme.halls <Graeme.Halls@nuance.com>
2024-09-16 12:08:18 +0100

    Adding Fluent UI Icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,24 @@
-import { useState } from 'react'
-import './App.css'
-import { FluentProvider, webLightTheme } from '@fluentui/react-components'
-import { Button } from '@fluentui/react-components'
+import { useState } from "react";
+import "./App.css";
+import { FluentProvider, webLightTheme } from "@fluentui/react-components";
+import { Button } from "@fluentui/react-components";
 
 function App() {
-
   return (
     <FluentProvider theme={webLightTheme}>
       <div className="App">
         <header className="App-header">
-          <p>
-            Unified Support
-          </p>
+          <p>Unified Support</p>
         </header>
+        <p>Get Started</p>
         <p>
-            Get Started
-          </p>
-          <p>
-           Welcome to the Unified Support App. Simply click "New Issue" to get started
-          </p>
-          <Button shape="circular">New Issue</Button>
+          Welcome to the Unified Support App. Simply click "New Issue" to get
+          started
+        </p>
+        <Button shape="circular" size="large">New Issue</Button>
       </div>
     </FluentProvider>
-  )
+  );
 }
 
-export default App
+export default App;
